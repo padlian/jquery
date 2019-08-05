87f1643ffd459659732c85c3e3a300f1acfc7622
padlian <padlian.chairi25@gmail.com>
2019-08-05 16:54:53 +0700

    pertemuan 3 chalange

--- pertemuan3/looping2.js
+++ pertemuan3/looping2.js
@@ -6,3 +6,43 @@ $(document).ready(function() {
 
  // if (i % 2 === 0) { /* we are even */ }
  //   else { /* we are odd */ }
+
+$(".loop").keyup(function(){
+	var awal = parseInt($('#awal').val());
+	var akhir = parseInt($('#akhir').val());
+
+	$('#target').html('');
+
+	for (var i = awal; i <= akhir; i++) {
+		var isi = i+"<br/>";
+    	$('#target').append(isi);
+    }
+});
+
+$("#tbl_genap").click(function(){
+	var awal = parseInt($('#awal').val());
+	var akhir = parseInt($('#akhir').val());
+
+	$('#target').html('');
+
+	for (var i = awal; i <= akhir; i++) {
+		if (i % 2 === 0){
+			var isi = i+"<br/>";
+	    	$('#target').append(isi);
+		}
+    }
+});
+
+$("#tbl_ganjil").click(function(){
+	var awal = parseInt($('#awal').val());
+	var akhir = parseInt($('#akhir').val());
+
+	$('#target').html('');
+
+	for (var i = awal; i <= akhir; i++) {
+		if (i % 2 === 1){
+			var isi = i+"<br/>";
+	    	$('#target').append(isi);
+		}
+    }
+});
